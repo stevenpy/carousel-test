cd328c62f7e1899f16aeefc1737c6197ebe57d07
Maxore <maxime.o@codium.co>
2014-10-03 17:47:16 +0700

    Scroll link done, load js separate file

---
+++ js/carousel.js
@@ -0,0 +1,33 @@
+$(function(){
+ $('.carousel').owlCarousel({
+  items: 4,
+  responsive: {
+    0: {
+      items: 1
+    },
+    480: {
+      items: 2,
+      margin: 15
+    },
+    768: {
+      items: 3,
+      margin: 30
+    },
+    992: {
+      items: 4,
+      margin: 45
+    }
+  }
+});
+});
+
+
+$(function(){
+ $('.services').owlCarousel({
+  items : 1, 
+  itemsDesktop : false,
+  itemsDesktopSmall : false,
+  itemsTablet: false,
+  itemsMobile : false
+});
+});
---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('a[href^="#"]').click(function(e) {
+        e.preventDefault();
+        var target = this.hash, $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 600, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
+});
